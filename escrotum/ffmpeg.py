e100f31cd376263e22cc1ed11223290d772877ff
Tiago Rinaldi <trinaldi@gmail.com>
2019-11-15 15:28:32 -0300

    Add reasonable ffmpeg args
    Assuming that 'hard part' is working, this commit add `ffmpeg` arguments
    based on similar software, guides and documentation.
    
    Sources are:
    
    + [`EasyScreenCast` GNOME Extension](https://github.com/EasyScreenCast/EasyScreenCast/blob/master/utilgsp.js#L79)
    + [Google guidelines for live encoding media](https://developers.google.com/media/vp9/live-encoding#choosing_encoding_parameters)
    + `ffmpeg` docs

--- escrotum/ffmpeg.py
+++ escrotum/ffmpeg.py
@@ -21,6 +21,9 @@ class Ffmpeg(object):
     def start(self):
         video_input = "%s+%s,%s" % (self.display, self.x, self.y)
         video_size = "%sx%s" % (self.w, self.h)
+        # Based on presets from
+        # EasyScreenCast GNOME Extension
+        # Google's Media Core Technologies Live Encoding examples
         cmd = [
             self.binary,
             '-loglevel', 'error',
@@ -30,6 +33,21 @@ class Ffmpeg(object):
             '-video_size', video_size,
             '-f', 'x11grab',
             '-i', video_input,
+            # Somewhere in the code the extension is `.mkv`. Assuming VP9.
+            # Google uses 'vp9' only
+            '-c:v', 'libvpx-vp9',
+            '-b:v', '1000k',
+            '-quality', 'realtime',
+            # Get threads automatically? Is it CPU threads?
+            '-threads', '8',
+            '-speed', '7',
+            '-row-mt', '1',
+            '-tile-columns', '3',
+            '-frame-parallel', '1',
+            '-qmin', '4',
+            '-qmax', '13',
+            '-r', '30',
+            '-g', '90',
             self.output]
         self.proc = subprocess.Popen(
             cmd,
